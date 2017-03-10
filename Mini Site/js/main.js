433cdf9991ee67593a4bf0a886acacc30c1487b6
Shilpa-Arora <shilpa.arora1994@gmail.com>
2017-03-09 22:26:23 -0500

    added files

---
+++ Mini Site/js/main.js
@@ -0,0 +1,23 @@
+(function() {
+"use strict";
+var controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave'
+			}
+		});
+
+		// get all slides
+		var slides = document.querySelectorAll("div.content");
+
+		// create scene for every slide
+		for (var i=0; i<slides.length; i++) {
+			new ScrollMagic.Scene({
+					triggerElement: slides[i]
+				})
+				.setPin(slides[i])
+				.addTo(controller);
+		}
+
+
+})();		
+
